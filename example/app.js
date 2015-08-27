0175e3dc9efc1e1eaddf3bb756cdf1cf4df7b325
lhamregit <lhamre@gmail.com>
2015-08-26 22:56:24 -0400

    new

--- example/app.js
+++ example/app.js
@@ -4,8 +4,8 @@ var app = express()
 
 // mount deeplinks middleware
 app.get('/deeplink', deeplink({
-  fallback: 'http://cupsapp.com',
-  android_package_name: 'com.citylifeapps.cups',
+  fallback: 'http://google.com',
+  android_package_name: 'com.wr1',
   ios_store_link: 'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4'
 }))
 
@@ -25,3 +25,20 @@ app.get('/another', function (req, res, next) {
   android_package_name: 'com.citylifeapps.cups',
   ios_store_link: 'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4'
 }))
+
+app.get('/wr1app', function (req, res, next) {
+  // you can do stuff here like add analytics to the request
+  // when you're done, simply pass on to deeplink:
+  // reformat the URL to fit deeplink lib
+  //http://wr1.com/wr1app?club=2&posttype=artistpost&post=140
+  //becomes vnd.wr1.wr1://com.wr1.ios/club/2/artistpost/140
+  var club = req.query.club;
+  var posttype = req.query.posttype;
+  var post = req.query.post;
+
+  // construct the new url and pass on
+  var url = 'vnd.wr1.wr1://com.wr1.ios/club/'+club+'/'+posttype+'/'+post
+  var redirectUrl = 'http://localhost:3000/deeplink?url=' + url;
+  console.log(redirectUrl)
+  res.redirect(redirectUrl);
+})
